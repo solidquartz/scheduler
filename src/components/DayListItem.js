efc7114ce5f33f7edc60f9698e502b4b1d57ac77
Aky Mac Donald <kotoko@Kotoko.local>
2022-07-13 17:44:20 -0400

    Added conditional function to show correct spots remaining text

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,28 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
+  const formatSpots = (props) => {
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`
+    }
+    if (props.spots === 1) {
+  return '1 spot remaining'
+    }
+    if (props.spots === 0) {
+      return 'no spots remaining'
+    }
+  };
+
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   });
 
+
   return (
     <li className={dayClass}  onClick={() => props.setDay(props.name)}>
       <h2>{props.name}</h2>
-      <h3>{props.spots} spots remaining</h3>
+      <h3>{formatSpots(props)}</h3>
     </li>
   );
 }
