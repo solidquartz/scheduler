2c4eae54ff8db3f2637d8d536b9a5ce5600c549b
Aky Mac Donald <kotoko@Kotoko.local>
2022-07-13 22:02:37 -0400

    Added DayList to display all days

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,19 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+
+  const days = props.days.map((day) => {
+    return (
+      <DayListItem
+        name={day.name}
+        spots={day.spots}
+      />
+    );
+  });
+
   return (
-    <ul></ul>
-  )
-}
+    <ul>
+      <li>{days}</li>
+    </ul>
+  );
+};
