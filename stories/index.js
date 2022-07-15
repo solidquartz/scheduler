f68515538b933425a7b78eb2988d090fbb908aaa
Aky Mac Donald <kotoko@Kotoko.local>
2022-07-14 23:22:57 -0400

    Add Show.js with code and stories

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,8 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js"
+import Show from "components/Appointment/Show.js"
+
 
 //Button
 storiesOf("Button", module)
@@ -165,5 +167,13 @@ storiesOf("Appointment", module)
   .add("Empty", () => (
     <Empty
       onAdd={action("onAdd")}
+    />
+  ))
+  .add("Show", () => (
+    <Show
+      student="Nozomi Tohjou"
+      interviewer={interviewer.name}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
     />
   ))
