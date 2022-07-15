4bb039ce508ef714308618f70d7c8480eecdeae9
Aky Mac Donald <kotoko@Kotoko.local>
2022-07-14 20:52:58 -0400

    Add Appointment function and scss

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "src/components/Appointment/styles.scss";
+
+export default function Appointment() {
+
+  return (
+    
+    <article className="appointment"></article>
+  )
+};
